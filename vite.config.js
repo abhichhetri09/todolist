8ddd590b045aed072eb2931f22c1d04093ac66ab
Abhishek chhetri <abhichhetri@outlook.com>
2023-10-26 21:50:46 +0300

    Deployed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todolist', // Add this row and use your own repository name
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
